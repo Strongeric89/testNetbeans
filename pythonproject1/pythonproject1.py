464766e3415d07a4f7ee1aceba673e882f50ca83
user <user@192.168.1.13>
2017-05-14 13:58:48 +0100

    commit 1

---
+++ pythonproject1/pythonproject1.py
@@ -0,0 +1,10 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+def main():
+    print('inside the main method now')
+
+
+
+if __name__ == "__main__":
+   main()
